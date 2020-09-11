7390e8f20791d4a4a5cd56112d4180ef383fb7ac
Luqman1102 <64735985+Luqman1102@users.noreply.github.com>
2020-09-11 14:43:42 +0700

    Menambahkan daftarProduk.php
    Menambahkan daftarProduk.php untuk list produk

---
+++ app/Views/daftarProduk.php
@@ -0,0 +1,169 @@
+<?= $this->extend('layout/template'); ?>
+<?= $this->section('content'); ?>
+<main> 
+    <div class="we-padding">
+        <div class="section-padding bg-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-25 col-md-3 col-sm-12">
+                        <div class="card mb-4 box-shadow produk">
+                            <div class="bg-light thumbnail-product">
+                                <a href="">
+                                    <img class="card-img-top h-100 product-cover" src="https://em.wattpad.com/306842f7a2c5107a19e24c54744055628479c0e7/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f43566258324764626d36696b45413d3d2d31362e313437663833356566396630313433343632353131323337363830372e6a7067?s=fit&w=720&h=720" data-holder-rendered="true">
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <a href="" class="card-text card-teks">This is a wider card with supporting</a>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between align-items-center">
+                                <small class="rating-wrapper">
+                                    <span class="star-rating-s15 show-one"></span>
+                                    <span class="rating-number">5.0</span>
+                                    <span class="text-muted">(999)</span>
+                                </small>
+                                <a href="" class="card-price">Rp 100.0000</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-25 col-md-3 col-sm-12">
+                        <div class="card mb-4 box-shadow produk">
+                            <div class="bg-light thumbnail-product">
+                                <a href="">
+                                    <img class="card-img-top h-100 product-cover" src="https://em.wattpad.com/306842f7a2c5107a19e24c54744055628479c0e7/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f43566258324764626d36696b45413d3d2d31362e313437663833356566396630313433343632353131323337363830372e6a7067?s=fit&w=720&h=720" data-holder-rendered="true">
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <a href="" class="card-text card-teks">This is a wider card with supporting</a>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between align-items-center">
+                                <small class="rating-wrapper">
+                                    <span class="star-rating-s15 show-one"></span>
+                                    <span class="rating-number">5.0</span>
+                                    <span class="text-muted">(999)</span>
+                                </small>
+                                <a href="" class="card-price">Rp 100.0000</a>
+                            </div>
+                        </div>
+                    </div><div class="col-lg-25 col-md-3 col-sm-12">
+                        <div class="card mb-4 box-shadow produk">
+                            <div class="bg-light thumbnail-product">
+                                <a href="">
+                                    <img class="card-img-top h-100 product-cover" src="https://em.wattpad.com/306842f7a2c5107a19e24c54744055628479c0e7/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f43566258324764626d36696b45413d3d2d31362e313437663833356566396630313433343632353131323337363830372e6a7067?s=fit&w=720&h=720" data-holder-rendered="true">
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <a href="" class="card-text card-teks">This is a wider card with supporting</a>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between align-items-center">
+                                <small class="rating-wrapper">
+                                    <span class="star-rating-s15 show-one"></span>
+                                    <span class="rating-number">5.0</span>
+                                    <span class="text-muted">(999)</span>
+                                </small>
+                                <a href="" class="card-price">Rp 100.0000</a>
+                            </div>
+                        </div>
+                    </div><div class="col-lg-25 col-md-3 col-sm-12">
+                        <div class="card mb-4 box-shadow produk">
+                            <div class="bg-light thumbnail-product">
+                                <a href="">
+                                    <img class="card-img-top h-100 product-cover" src="https://em.wattpad.com/306842f7a2c5107a19e24c54744055628479c0e7/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f43566258324764626d36696b45413d3d2d31362e313437663833356566396630313433343632353131323337363830372e6a7067?s=fit&w=720&h=720" data-holder-rendered="true">
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <a href="" class="card-text card-teks">This is a wider card with supporting</a>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between align-items-center">
+                                <small class="rating-wrapper">
+                                    <span class="star-rating-s15 show-one"></span>
+                                    <span class="rating-number">5.0</span>
+                                    <span class="text-muted">(999)</span>
+                                </small>
+                                <a href="" class="card-price">Rp 100.0000</a>
+                            </div>
+                        </div>
+                    </div><div class="col-lg-25 col-md-3 col-sm-12">
+                        <div class="card mb-4 box-shadow produk">
+                            <div class="bg-light thumbnail-product">
+                                <a href="">
+                                    <img class="card-img-top h-100 product-cover" src="https://em.wattpad.com/306842f7a2c5107a19e24c54744055628479c0e7/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f43566258324764626d36696b45413d3d2d31362e313437663833356566396630313433343632353131323337363830372e6a7067?s=fit&w=720&h=720" data-holder-rendered="true">
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <a href="" class="card-text card-teks">This is a wider card with supporting</a>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between align-items-center">
+                                <small class="rating-wrapper">
+                                    <span class="star-rating-s15 show-one"></span>
+                                    <span class="rating-number">5.0</span>
+                                    <span class="text-muted">(999)</span>
+                                </small>
+                                <a href="" class="card-price">Rp 100.0000</a>
+                            </div>
+                        </div>
+                    </div><div class="col-lg-25 col-md-3 col-sm-12">
+                        <div class="card mb-4 box-shadow produk">
+                            <div class="bg-light thumbnail-product">
+                                <a href="">
+                                    <img class="card-img-top h-100 product-cover" src="https://em.wattpad.com/306842f7a2c5107a19e24c54744055628479c0e7/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f43566258324764626d36696b45413d3d2d31362e313437663833356566396630313433343632353131323337363830372e6a7067?s=fit&w=720&h=720" data-holder-rendered="true">
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <a href="" class="card-text card-teks">This is a wider card with supporting</a>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between align-items-center">
+                                <small class="rating-wrapper">
+                                    <span class="star-rating-s15 show-one"></span>
+                                    <span class="rating-number">5.0</span>
+                                    <span class="text-muted">(999)</span>
+                                </small>
+                                <a href="" class="card-price">Rp 100.0000</a>
+                            </div>
+                        </div>
+                    </div><div class="col-lg-25 col-md-3 col-sm-12">
+                        <div class="card mb-4 box-shadow produk">
+                            <div class="bg-light thumbnail-product">
+                                <a href="">
+                                    <img class="card-img-top h-100 product-cover" src="https://em.wattpad.com/306842f7a2c5107a19e24c54744055628479c0e7/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f43566258324764626d36696b45413d3d2d31362e313437663833356566396630313433343632353131323337363830372e6a7067?s=fit&w=720&h=720" data-holder-rendered="true">
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <a href="" class="card-text card-teks">This is a wider card with supporting</a>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between align-items-center">
+                                <small class="rating-wrapper">
+                                    <span class="star-rating-s15 show-one"></span>
+                                    <span class="rating-number">5.0</span>
+                                    <span class="text-muted">(999)</span>
+                                </small>
+                                <a href="" class="card-price">Rp 100.0000</a>
+                            </div>
+                        </div>
+                    </div><div class="col-lg-25 col-md-3 col-sm-12">
+                        <div class="card mb-4 box-shadow produk">
+                            <div class="bg-light thumbnail-product">
+                                <a href="">
+                                    <img class="card-img-top h-100 product-cover" src="https://em.wattpad.com/306842f7a2c5107a19e24c54744055628479c0e7/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f43566258324764626d36696b45413d3d2d31362e313437663833356566396630313433343632353131323337363830372e6a7067?s=fit&w=720&h=720" data-holder-rendered="true">
+                                </a>
+                            </div>
+                            <div class="card-body">
+                                <a href="" class="card-text card-teks">This is a wider card with supporting</a>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between align-items-center">
+                                <small class="rating-wrapper">
+                                    <span class="star-rating-s15 show-one"></span>
+                                    <span class="rating-number">5.0</span>
+                                    <span class="text-muted">(999)</span>
+                                </small>
+                                <a href="" class="card-price">Rp 100.0000</a>
+                            </div>
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+    </div>
+
+</main>
+
+<?= $this->endSection(); ?>
